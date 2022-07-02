1475599eea99a9a002ee4cb41591b87b21950bd6
shuyijia <72814243+shuyijia@users.noreply.github.com>
2022-07-03 00:52:21 +0800

    VSMIndex for SQuAD

---
+++ index/utils.py
@@ -0,0 +1,17 @@
+# preprocess methods
+def punctuation_removal(s):
+  return re.sub(r'[^\w\s]', '', s)
+
+def case_folding(s, case='lower'):
+  return s.lower() if case == 'lower' else s.upper()
+
+def split_and_remove_spaces(s):
+  return [w for w in s.split(' ') if w != '']
+
+def preprocess(l, remove_punctuation=True, fold_cases=True):
+  if remove_punctuation:
+    l = map(punctuation_removal, l)
+  if fold_cases:
+    l = map(case_folding, l)
+  
+  return list(map(split_and_remove_spaces, l))
---
+++ index/vsm.py
@@ -0,0 +1,46 @@
+import gensim, re
+from gensim.test.utils import common_texts
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+from utils import *
+
+class VSMIndex:
+  def __init__(self, method, dataset):
+    self.method = method
+    self.dataset = dataset
+    self.contexts = preprocess(list(set(self.dataset['context'])))
+
+    if self.method == 'doc2vec':
+      self.gen_doc2vec()
+    elif self.method == 'tfidf':
+      self.gen_tfidf()
+    else:
+      raise ValueError('Unsppported method type.')
+  
+  def gen_doc2vec(self, vector_size=40, min_count=2, epochs=30):
+    documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(self.contexts)]
+    self.model = gensim.models.doc2vec.Doc2Vec(documents, vector_size=vector_size, min_count=min_count, epochs=epochs)
+    
+    # use trained doc2vec to represent docs
+    self.doc_vecs = np.zeros((len(self.contexts), vector_size))
+    for i, doc in enumerate(self.contexts):
+      self.doc_vecs[i, :] = self.model.infer_vector(doc)
+
+  def gen_tfidf(self, max_features=500):
+    documents = [' '.join(doc) for doc in self.contexts]
+    self.model = TfidfVectorizer(max_features=max_features)
+    self.X = self.model.fit_transform(documents)
+
+    self.doc_vecs = np.zeros((len(self.contexts), max_features))
+    for i, doc in enumerate(documents):
+      self.doc_vecs[i, :] = self.model.transform([doc]).todense()
+
+  def infer(self, doc):
+    '''
+    doc: list of words
+    '''
+    if self.method == 'doc2vec':
+      return self.model.infer_vector(doc)
+    elif self.method == 'tfidf':
+      return self.model.transform(' '.join(doc))
