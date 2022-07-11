af06eca9454dab3da46ff6438a9afd603258ef82
shuyijia <shuyi0810@gmail.com>
2022-07-11 15:51:28 +0800

    created indices for both bm25 and vsm

---
+++ index/bm25.py
@@ -0,0 +1,39 @@
+import datasets
+import numpy as np
+from utils import *
+from datasets import load_dataset
+
+class BM25Index:
+    def __init__(self, dataset) -> None:
+        self.dataset = dataset
+        self.contexts = preprocess(list(set(self.dataset['context'])))
+
+        self.build()
+
+    def build(self):
+        '''
+        build an index for term frequency and document frequency
+        word -> {doc1 : 10, doc2 : 3, ..., doc_n : 23}
+        '''
+        index = {}
+
+        for i, doc in enumerate(self.contexts):
+            for term in doc:
+                if term not in index:
+                    index[term] = {}
+                if i not in index[term]:
+                    index[term][i] = 0
+                index[term][i] += 1
+        
+        self.index = index
+
+if __name__ == "__main__":
+    datasets = load_dataset("squad_v2")
+    valid = datasets['validation']
+    idx = BM25Index(valid)
+
+    print("number of unique docs: ", len(idx.contexts))
+
+    word = 'hydrogen'
+    print('tf in all docs containing {}: {}'.format(word, idx.index[word]))
+    print('df of {}: {}'.format(word, len(idx.index[word])))
--- index/vsm.py
+++ index/vsm.py
@@ -1,45 +1,64 @@
-import gensim
+from datasets import load_dataset
 import numpy as np
 from utils import *
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 class VSMIndex:
-  def __init__(self, method, dataset):
-    self.method = method
-    self.dataset = dataset
-    self.contexts = preprocess(list(set(self.dataset['context'])))
+    def __init__(self, method, dataset, vector_size=40):
+        self.method = method
+        self.dataset = dataset
+        self.vector_size = vector_size
+        self.contexts = preprocess(list(set(self.dataset['context'])))
 
-    if self.method == 'doc2vec':
-      self.gen_doc2vec()
-    elif self.method == 'tfidf':
-      self.gen_tfidf()
-    else:
-      raise ValueError('Unsppported method type.')
-  
-  def gen_doc2vec(self, vector_size=40, min_count=2, epochs=30):
-    documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(self.contexts)]
-    self.model = Doc2Vec(documents, vector_size=vector_size, min_count=min_count, epochs=epochs)
-    
-    # use trained doc2vec to represent docs
-    self.doc_vecs = np.zeros((len(self.contexts), vector_size))
-    for i, doc in enumerate(self.contexts):
-      self.doc_vecs[i, :] = self.model.infer_vector(doc)
+        if self.method == 'doc2vec':
+            self.gen_doc2vec(vector_size=vector_size)
+        elif self.method == 'tfidf':
+            self.gen_tfidf()
+        else:
+            raise ValueError('Unsppported method type.')
 
-  def gen_tfidf(self, max_features=500):
-    documents = [' '.join(doc) for doc in self.contexts]
-    self.model = TfidfVectorizer(max_features=max_features)
-    self.X = self.model.fit_transform(documents)
+    def gen_doc2vec(self, vector_size=40, min_count=2, epochs=30):
+        documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(self.contexts)]
+        self.model = Doc2Vec(documents, vector_size=vector_size, min_count=min_count, epochs=epochs)
 
-    self.doc_vecs = np.zeros((len(self.contexts), max_features))
-    for i, doc in enumerate(documents):
-      self.doc_vecs[i, :] = self.model.transform([doc]).todense()
+        # use trained doc2vec to represent docs
+        self.doc_vecs = np.zeros((len(self.contexts), vector_size))
+        for i, doc in enumerate(self.contexts):
+            self.doc_vecs[i, :] = self.model.infer_vector(doc)
 
-  def infer(self, doc):
-    '''
-    doc: list of words
-    '''
-    if self.method == 'doc2vec':
-      return self.model.infer_vector(doc)
-    elif self.method == 'tfidf':
-      return self.model.transform(' '.join(doc))
+    def gen_tfidf(self, max_features=500):
+        documents = [' '.join(doc) for doc in self.contexts]
+        self.model = TfidfVectorizer(max_features=max_features)
+        self.X = self.model.fit_transform(documents)
+
+        self.doc_vecs = np.zeros((len(self.contexts), max_features))
+        for i, doc in enumerate(documents):
+            self.doc_vecs[i, :] = self.model.transform([doc]).todense()
+
+    def infer(self, doc):
+        '''
+        doc: list of words
+        '''
+        if self.method == 'doc2vec':
+            return self.model.infer_vector(doc)
+        elif self.method == 'tfidf':
+            return self.model.transform(' '.join(doc))
+
+
+if __name__ == '__main__':
+    datasets = load_dataset("squad_v2")
+    valid = datasets['validation']
+
+    method = 'doc2vec'
+
+    idx = VSMIndex(method, valid)
+
+    # get all vectorzied documents
+    all_docs = idx.doc_vecs
+
+    # convert a query to vectorized form
+    query = 'what is the name of the last French king'.split(' ')
+    vectorized_query = idx.infer(query)
+    print(vectorized_query.shape)
